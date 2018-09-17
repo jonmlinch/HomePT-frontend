68ec4c85215ce22db8e54e576d9db97486381289
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-09-17 15:18:28 -0700

    Created initial auth/routes in App component. Still needs coresponding components for some routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowerRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
+import SERVER_URL from './constants/server';
 import './App.css';
+import Home from './Home';
+import Login from './auth/Login';
+import Nav from './layout/Nav';
+import Profile from './Profile';
+import Signup from './auth/Signup';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        user: null
+      }
+    }
+
+  componentDidMount = () => {
+    console.log('component did mount!');
+    this.getUser();
+  }
+
+  getUser = () => {
+    var token = localStorage.getItem('mernToken');
+    if(token){
+      console.log('token found in local storage', token);
+      //There is a token in localStorage ****This Needs to change to cookies ****
+      axios.post(SERVER_URL + '/auth/me/from/token', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      }).then(response => {
+        console.log('SUCCESS', respnse);
+        this.setState({
+          user: respnse.data.user
+        });
+      }).catch( err => {
+        console.log('ERROR', err);
+        console.log('response', err.response);
+        localStorage.removeItem('mernToken'); //This will need to change when using Cookies
+        this.setState({
+          user: null
+        });
+      });
+    } else {
+      console.log('No token was found');
+      localStorage.removeItem('mernToken');
+      this.setState({
+        user: null
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div className="container">
+          <Nav user={this.state.user} updateUser={this.getUser} />
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={
+            () => (<Login user={this.state.user} updateUser={this.getUser} />)
+          } />
+          <Route path="/signup" component={
+            () => (<Signup user={this.state.user} updateUser={this.getUser} />)
+          } />
+          <Route path="/profile" component={
+            () => (<Profile user={this.state.user} updateUser={this.getUser} />)
+          } />
+          </div>
+        </Router>
       </div>
     );
   }
